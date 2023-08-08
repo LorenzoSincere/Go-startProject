30d4a6d7b7af4e6020af182a8a5984b06cd00b5b
Lorenzo-Creativity <1604763055@qq.com>
2023-08-08 23:54:51 +0800

    v2.5 结构体&明天修bug

--- go-project-example/Service/query_page_info.go
+++ go-project-example/Service/query_page_info.go
@@ -11,3 +11,26 @@ type PostInfo struct {
 	Post *Repository.Post
 	User *Repository.User
 }
+
+type PageInfo struct {
+	TopicInfo *TopicInfo
+	PostList  []*PostInfo
+}
+
+type QueryPageInfoFlow struct {
+	topicId  int64
+	pageInfo *PageInfo
+	topic    *Repository.Topic
+	posts    []*Repository.Post
+	userMap  map[int64]*Repository.User
+}
+
+func QueryPageInfo(topicId int64) (*PageInfo, error) {
+	return NewQueryPageInfoFlow(topicId).Do(), nil
+}
+
+func NewQueryPageInfoFlow(topId int64) *QueryPageInfoFlow {
+	return &QueryPageInfoFlow{
+		topicId: topId,
+	}
+}
