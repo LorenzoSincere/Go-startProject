c6b0acb7f4772b144dbc3a2db84011426c5077a6
Lorenzo-Creativity <1604763055@qq.com>
2023-08-09 23:21:40 +0800

    v2.8.2 新增基准测试

--- go-project-example/Service/publish_post_test.go
+++ go-project-example/Service/publish_post_test.go
@@ -49,3 +49,40 @@ func TestPublishPost(t *testing.T) {
 		})
 	}
 }
+
+func BenchmarkPublishPost(b *testing.B) {
+	type args struct {
+		topicId int64
+		userId  int64
+		content string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "测试发布回帖",
+			args: args{
+				topicId: 1,
+				userId:  2,
+				content: "再次回帖",
+			},
+			wantErr: false,
+		},
+	}
+
+	b.ResetTimer()
+	for _, tt := range tests {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				_, err := PublishPost(tt.args.topicId, tt.args.userId, tt.args.content)
+				if (err != nil) != tt.wantErr {
+					b.Errorf("publishPost() error: %v, wantError: %v", err, tt.wantErr)
+					return
+				}
+			}
+
+		})
+	}
+}
