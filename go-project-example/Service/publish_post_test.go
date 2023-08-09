7530771a718bef8b257bd8d063b306d9dea84366
Lorenzo-Creativity <1604763055@qq.com>
2023-08-09 23:04:30 +0800

    v2.8.1 修复bug

--- go-project-example/Service/publish_post_test.go
+++ go-project-example/Service/publish_post_test.go
@@ -1,1 +1,51 @@
 package Service
+
+import (
+	"os"
+	"startProject/go-project-example/Repository"
+	"startProject/go-project-example/Util"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	if err := Repository.Init(); err != nil {
+		os.Exit(1)
+	}
+	if err := Util.InitLogger(); err != nil {
+		os.Exit(1)
+	}
+	m.Run()
+}
+
+func TestPublishPost(t *testing.T) {
+	type args struct {
+		topicId int64
+		userId  int64
+		content string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "测试发布回帖",
+			args: args{
+				topicId: 1,
+				userId:  2,
+				content: "再次回帖",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := PublishPost(tt.args.topicId, tt.args.userId, tt.args.content)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("publishPost() error: %v, wantError: %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
