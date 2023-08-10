a537fdb47cb68f84a3a902a4527c88381c8543d0
Lorenzo-Creativity <1604763055@qq.com>
2023-08-10 23:43:10 +0800

    v2.8.4 完善新增handle

--- go-project-example/handler/publish_post.go
+++ go-project-example/handler/publish_post.go
@@ -1,6 +1,27 @@
 package handler
 
+import (
+	"startProject/go-project-example/Service"
+	"strconv"
+)
+
 func PublishPost(uidStr, topicIdStr, content string) *PageData {
 	//参数转换
-	uid, _ := strocv
+	uid, _ := strconv.ParseInt(uidStr, 10, 64)
+	topic, _ := strconv.ParseInt(topicIdStr, 10, 64)
+	//获取service层结果
+	postId, err := Service.PublishPost(topic, uid, content)
+	if err != nil {
+		return &PageData{
+			Code: -1,
+			Msg:  err.Error(),
+		}
+	}
+	return &PageData{
+		Code: 0,
+		Msg:  "success",
+		Data: map[string]int64{
+			"postId": postId,
+		},
+	}
 }
