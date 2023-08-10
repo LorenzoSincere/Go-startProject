e73f8df581fe0e3cf05f1c27a6be634443f1edc1
Lorenzo-Creativity <1604763055@qq.com>
2023-08-10 23:35:43 +0800

    v2.8.3 完善查询handle

--- go-project-example/handler/query_page_info.go
+++ go-project-example/handler/query_page_info.go
@@ -1,1 +1,37 @@
 package handler
+
+import (
+	"startProject/go-project-example/Service"
+	"strconv"
+)
+
+type PageData struct {
+	Code int64       `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func QueryPageInfo(topicIdStr string) *PageData {
+	//参数转换
+	topicId, err := strconv.ParseInt(topicIdStr, 10, 64)
+	if err != nil {
+		return &PageData{
+			Code: -1,
+			Msg:  err.Error(),
+		}
+	}
+	//获取service层结果
+	pageInfo, err := Service.QueryPageInfo(topicId)
+	if err != nil {
+		return &PageData{
+			Code: -1,
+			Msg:  err.Error(),
+		}
+	}
+	return &PageData{
+		Code: 0,
+		Msg:  "success",
+		Data: pageInfo,
+	}
+
+}
